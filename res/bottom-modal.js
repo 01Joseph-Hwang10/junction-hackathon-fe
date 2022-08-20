bf648a4bf0f119906e4bd6e09eea3ac72b7176a3
01Joseph-Hwang10 <joseph95501@gmail.com>
2022-08-20 20:18:32 +0900

    add select sow

--- res/bottom-modal.js
+++ res/bottom-modal.js
@@ -18,31 +18,50 @@ var clear = function () {
 var show = function (selector) {
     document.querySelector(selector).setAttribute("style", "display: flex;");
 };
+var showBackButton = function (info) {
+    document.querySelectorAll('#indicator .side').forEach(function (side) {
+        side.setAttribute('style', 'display: block;');
+    });
+    document.querySelector('#indicator').removeAttribute('style');
+    var backButton = document.querySelector('#indicator .back');
+    backButton.onclick = function () { return registerSelectStatusUI(info); };
+};
+var hideBackButton = function () {
+    document.querySelectorAll('#indicator .side').forEach(function (side) {
+        side.setAttribute('style', 'display: none;');
+    });
+    document.querySelector('#indicator').setAttribute('style', 'justify-content: center;');
+};
 var setIndicator = function (statement) {
-    var indicator = document.querySelector("#indicator");
+    var indicator = document.querySelector("#indicator .title");
     indicator.innerText = statement;
 };
 var getStatusStatement = function (crop) { return "".concat(crop, "\uC744 \uC2EC\uC740 \uB18D\uC9C0"); };
 var getButtonCard = function () {
     return document.querySelector("#button-card");
 };
+var getOption = function () { return document.querySelector('#option'); };
 var cropSelection = {
     Japonica: "쌀",
     Tomato: "토마토",
     Corn: "옥수수"
 };
 var appendButtonCards = function (items, to) {
+    // Clear cards
+    var cards = to.querySelectorAll(".card");
+    cards.forEach(function (card) { return card.remove(); });
+    // Attach cards
     var buttonCard = getButtonCard();
-    for (var _i = 0, items_1 = items; _i < items_1.length; _i++) {
-        var card = items_1[_i];
+    for (var _i = 0, _a = Object.keys(items); _i < _a.length; _i++) {
+        var card = _a[_i];
         var clone = document.importNode(buttonCard.content, true);
         var button = clone.querySelector(".card");
         button.setAttribute("to", card);
         button.setAttribute("style", "width: ".concat(100 / items.length, "%;"));
         var span = clone.querySelector("span");
-        span.innerText = card;
-        var cards = to.querySelector(".cards");
-        cards.appendChild(clone);
+        span.innerText = items[card];
+        var cards_1 = to.querySelector(".cards");
+        cards_1.appendChild(clone);
     }
     var buttons = [];
     to.querySelectorAll("button.card").forEach(function (button) {
@@ -50,25 +69,23 @@ var appendButtonCards = function (items, to) {
     });
     return buttons;
 };
-var registerCropSelectionUI = function () {
+var registerCropSelectionUI = function (range) {
     // Initialize UI
     clear();
     show("#select-crop");
+    hideBackButton();
     // Indicator
     setIndicator("비어있는 농지");
     // Crop Selection
     var selectCrop = document.querySelector("#select-crop");
-    var buttons = appendButtonCards(Object.keys(cropSelection), selectCrop);
+    var buttons = appendButtonCards(cropSelection, selectCrop);
     buttons.forEach(function (button) {
         button.onclick = function () {
             var crop = button.getAttribute("to");
             registerSelectStatusUI({
                 crop: crop,
+                range: range,
                 progress: 0,
-                range: {
-                    x: [0, 10],
-                    y: [0, 10]
-                },
                 id: Math.random().toString()
             });
         };
@@ -85,14 +102,15 @@ var registerSelectStatusUI = function (info) {
     // Initialize UI
     clear();
     show("#select-status");
+    hideBackButton();
     // Title Statement
-    setIndicator(getStatusStatement(info.crop));
+    setIndicator(getStatusStatement(cropSelection[info.crop]));
     // Progress
     var selectStatus = document.querySelector("#select-status");
     var progress = selectStatus.querySelector("progress");
     progress.value = info.progress * 100;
     // Actions
-    var buttons = appendButtonCards(Object.keys(actions), selectStatus);
+    var buttons = appendButtonCards(actions, selectStatus);
     var sowButton = buttons[0], irrigationButton = buttons[1], topDressingButton = buttons[2], plowingButton = buttons[3], harvestButton = buttons[4];
     sowButton.onclick = function () { return registerSelectSowUI(info); };
     irrigationButton.onclick = function () { return registerSelectIrrigationUI(info); };
@@ -100,15 +118,99 @@ var registerSelectStatusUI = function (info) {
     plowingButton.onclick = function () { return registerSelectPlowingButtonUI(info); };
     harvestButton.onclick = function () { }; // Some Action when harvest
 };
+var sowMethod = {
+    'direct': '직접',
+    'indirect': '간접'
+};
+var sowGap = {
+    'narrow': '좁게',
+    'wide': '넓게'
+};
+var appendOptions = function (options, to) {
+    // Clear options
+    var optionsElement = to.querySelectorAll("option");
+    optionsElement.forEach(function (option, index) {
+        if (index === 0) {
+            option.selected = true;
+        }
+        else {
+            option.remove();
+        }
+    });
+    // Append options
+    var optionTemplate = getOption();
+    for (var _i = 0, _a = Object.keys(options); _i < _a.length; _i++) {
+        var item = _a[_i];
+        var clone = document.importNode(optionTemplate.content, true);
+        var span = clone.querySelector("span");
+        span.innerText = options[item];
+        var option = clone.querySelector("option");
+        option.value = item;
+        to.appendChild(clone);
+    }
+    var opts = [];
+    to.querySelectorAll("option").forEach(function (opt) {
+        return opts.push(opt);
+    });
+    return opts;
+};
 var registerSelectSowUI = function (info) {
     // Initialize UI
     clear();
     show("#select-sow");
+    showBackButton(info);
+    var selectSow = document.querySelector("#select-sow");
+    // Sow methods
+    var sowMethodSelection = selectSow.querySelector('#sow-method');
+    var sowMethodOptions = appendOptions(sowMethod, sowMethodSelection);
+    // Sow Gap
+    var sowGapSelection = selectSow.querySelector('#sow-gap');
+    appendOptions(sowGap, sowGapSelection);
+    var sowGapOptions = sowGapSelection.querySelectorAll("option");
+    // Sow Degree
+    var sowDegreeIndicator = selectSow.querySelector("#sow-degree span");
+    var sowDegreeConfig = selectSow.querySelector('#sow-degree input');
+    sowDegreeConfig.oninput = function (event) {
+        var degree = event.target.value.toString();
+        if (Number(event.target.value) < 10) {
+            degree = '&nbsp;&nbsp;' + degree;
+        }
+        else if (Number(event.target.value) < 100) {
+            degree = '&nbsp;' + degree;
+        }
+        else {
+            // pass
+        }
+        sowDegreeIndicator.innerHTML = "\uD30C\uC885 \uAC01\uB3C4: ".concat(degree);
+    };
+    if (info.sow) {
+        sowMethodOptions.forEach(function (option) {
+            if (option.value === info.sow.method) {
+                option.setAttribute("selected", "true");
+            }
+            else {
+                option.removeAttribute("selected");
+            }
+        });
+        sowGapOptions.forEach(function (option) {
+            if (option.value === info.sow.gap) {
+                option.setAttribute("selected", "true");
+            }
+            else {
+                option.removeAttribute("selected");
+            }
+        });
+        sowDegreeConfig.value = info.sow.degree.toString();
+        sowDegreeIndicator.innerText = "\uD30C\uC885 \uAC01\uB3C4: ".concat(info.sow.degree);
+    }
 };
 var registerSelectIrrigationUI = function (info) { };
 var registerSelectTopDressingUI = function (info) { };
 var registerSelectPlowingButtonUI = function (info) { };
-registerCropSelectionUI();
+registerCropSelectionUI({
+    x: [0, 10],
+    y: [0, 10]
+});
 var getTimeDom = function () {
     return document.querySelector(".time-text");
 };
--- src/bottom-modal.ts
+++ src/bottom-modal.ts
@@ -10,7 +10,9 @@
     - #select-harvest
 */
 
-import { ActionType, CropType, LandTileInfo } from "./types";
+import { ActionType, CropType, LandTileInfo, SowGap, SowMethod } from "./types";
+
+/**==============> Utilities ================>*/
 
 const clear = () => {
   document.querySelectorAll("section").forEach((section) => {
@@ -22,8 +24,24 @@ const show = (selector: string) => {
   document.querySelector(selector).setAttribute("style", "display: flex;");
 };
 
+const showBackButton = (info: LandTileInfo) => {
+  document.querySelectorAll('#indicator .side').forEach((side) => {
+    side.setAttribute('style', 'display: block;')
+  })
+  document.querySelector('#indicator').removeAttribute('style')
+  const backButton: HTMLHeadingElement = document.querySelector('#indicator .back')
+  backButton.onclick = () => registerSelectStatusUI(info)
+}
+
+const hideBackButton = () => {
+  document.querySelectorAll('#indicator .side').forEach((side) => {
+    side.setAttribute('style', 'display: none;')
+  })
+  document.querySelector('#indicator').setAttribute('style', 'justify-content: center;')
+}
+
 const setIndicator = (statement: string) => {
-  const indicator: HTMLHeadingElement = document.querySelector("#indicator");
+  const indicator: HTMLHeadingElement = document.querySelector("#indicator .title");
   indicator.innerText = statement;
 };
 
@@ -32,59 +50,16 @@ const getStatusStatement = (crop: string) => `${crop}을 심은 농지`;
 const getButtonCard = (): HTMLTemplateElement =>
   document.querySelector("#button-card");
 
+const getOption = (): HTMLTemplateElement => document.querySelector('#option')
+
+/**<============== End Utilities <================*/
+
+/**==============> Constants ================>*/
+
 const cropSelection: Record<CropType, string> = {
   Japonica: "쌀",
   Tomato: "토마토",
   Corn: "옥수수",
-};
-
-const appendButtonCards = <T extends string = string>(
-  items: T[],
-  to: Element
-): HTMLButtonElement[] => {
-  const buttonCard = getButtonCard();
-  for (const card of items) {
-    const clone = document.importNode(buttonCard.content, true);
-    const button: HTMLButtonElement = clone.querySelector(".card");
-    button.setAttribute("to", card);
-    button.setAttribute("style", `width: ${100 / items.length}%;`);
-    const span = clone.querySelector("span");
-    span.innerText = card;
-    const cards = to.querySelector(".cards");
-    cards.appendChild(clone);
-  }
-  const buttons: HTMLButtonElement[] = [];
-  to.querySelectorAll("button.card").forEach((button: HTMLButtonElement) =>
-    buttons.push(button)
-  );
-  return buttons;
-};
-
-const registerCropSelectionUI = () => {
-  // Initialize UI
-  clear();
-  show("#select-crop");
-
-  // Indicator
-  setIndicator("비어있는 농지");
-
-  // Crop Selection
-  const selectCrop = document.querySelector("#select-crop");
-  const buttons = appendButtonCards(Object.keys(cropSelection), selectCrop);
-  buttons.forEach((button) => {
-    button.onclick = () => {
-      const crop: CropType = button.getAttribute("to") as CropType;
-      registerSelectStatusUI({
-        crop,
-        progress: 0,
-        range: {
-          x: [0, 10],
-          y: [0, 10],
-        },
-        id: Math.random().toString(),
-      });
-    };
-  });
 };
 
 const actions: Record<ActionType, string> = {
@@ -95,13 +70,117 @@ const actions: Record<ActionType, string> = {
   harvest: "수확",
 };
 
+const sowMethod: Record<SowMethod, string> = {
+  'direct': '직접',
+  'indirect': '간접',
+}
+
+const sowGap: Record<SowGap, string> = {
+  'narrow': '좁게',
+  'wide': '넓게',
+}
+
+/**<============== End Constants <================*/
+
+/**==============> Appenders ================>*/
+
+const appendButtonCards = <T extends Record<any, any>>(
+  items: T,
+  to: Element
+): HTMLButtonElement[] => {
+  // Clear cards
+  const cards = to.querySelectorAll(".card");
+  cards.forEach(card => card.remove())
+
+  // Attach cards
+  const buttonCard = getButtonCard();
+  for (const card of Object.keys(items)) {
+    const clone = document.importNode(buttonCard.content, true);
+    const button: HTMLButtonElement = clone.querySelector(".card");
+    button.setAttribute("to", card);
+    button.setAttribute("style", `width: ${100 / items.length}%;`);
+    const span = clone.querySelector("span");
+    span.innerText = items[card];
+    const cards = to.querySelector(".cards");
+    cards.appendChild(clone);
+  }
+  const buttons: HTMLButtonElement[] = [];
+  to.querySelectorAll("button.card").forEach((button: HTMLButtonElement) =>
+    buttons.push(button)
+  );
+  return buttons;
+};
+
+
+const appendOptions = <T extends Record<any, any>>(options: T, to: Element): HTMLOptionElement[] => {
+  // Clear options
+  const optionsElement = to.querySelectorAll("option");
+  optionsElement.forEach((option, index) => {
+    if (index === 0) {
+      option.selected = true
+    } else {
+      option.remove()
+    }
+  })
+
+  // Append options
+  const optionTemplate = getOption();
+  for (const item of Object.keys(options)) {
+    const clone = document.importNode(optionTemplate.content, true);
+    const span = clone.querySelector("span");
+    span.innerText = options[item];
+    const option = clone.querySelector("option");
+    option.value = item;
+    to.appendChild(clone);
+  }
+  const opts: HTMLOptionElement[] = [];
+  to.querySelectorAll("option").forEach((opt: HTMLOptionElement) =>
+    opts.push(opt)
+  );
+  return opts
+}
+
+/**<============== End Appenders <================*/
+
+/**==============> CropSelection ================>*/
+
+const registerCropSelectionUI = (range: LandTileInfo['range']) => {
+  // Initialize UI
+  clear();
+  show("#select-crop");
+  hideBackButton()
+
+  // Indicator
+  setIndicator("비어있는 농지");
+
+  // Crop Selection
+  const selectCrop = document.querySelector("#select-crop");
+  const buttons = appendButtonCards(cropSelection, selectCrop);
+  buttons.forEach((button) => {
+    button.onclick = () => {
+      const crop: CropType = button.getAttribute("to") as CropType;
+      registerSelectStatusUI({
+        crop,
+        range,
+        progress: 0,
+        id: Math.random().toString(),
+      });
+    };
+  });
+};
+
+/**<============== End CropSelection <================*/
+
+/**==============> Select Status ================>*/
+
 const registerSelectStatusUI = (info: LandTileInfo) => {
   // Initialize UI
   clear();
   show("#select-status");
+  hideBackButton()
 
   // Title Statement
-  setIndicator(getStatusStatement(info.crop));
+  setIndicator(getStatusStatement(cropSelection[info.crop]));
 
   // Progress
   const selectStatus = document.querySelector("#select-status");
@@ -109,28 +188,93 @@ const registerSelectStatusUI = (info: LandTileInfo) => {
   progress.value = info.progress * 100;
 
   // Actions
-  const buttons = appendButtonCards(Object.keys(actions), selectStatus)
+  const buttons = appendButtonCards(actions, selectStatus)
   const [sowButton, irrigationButton, topDressingButton, plowingButton, harvestButton] = buttons
   sowButton.onclick = () => registerSelectSowUI(info)
   irrigationButton.onclick = () => registerSelectIrrigationUI(info)
   topDressingButton.onclick = () => registerSelectTopDressingUI(info)
-  plowingButton.onclick = () => registerSelectPlowingButtonUI(info)
+  plowingButton.onclick = () => {} // Some Action when plowing
   harvestButton.onclick = () => {} // Some Action when harvest
 }
+
+/**<============== End Select Status <================*/
+
 
 const registerSelectSowUI = (info: LandTileInfo) => {
   // Initialize UI
   clear();
   show("#select-sow");
+  showBackButton(info)
+
+  const selectSow = document.querySelector("#select-sow");
+  // Sow methods
+  const sowMethodSelection = selectSow.querySelector('#sow-method');
+  const sowMethodOptions = appendOptions(sowMethod, sowMethodSelection);
+
+  // Sow Gap
+  const sowGapSelection = selectSow.querySelector('#sow-gap');
+  appendOptions(sowGap, sowGapSelection);
+  const sowGapOptions = sowGapSelection.querySelectorAll("option");
+
+  // Sow Degree
+  const sowDegreeIndicator: HTMLSpanElement = selectSow.querySelector("#sow-degree span");
+  const sowDegreeConfig: HTMLInputElement = selectSow.querySelector('#sow-degree input')
+  sowDegreeConfig.oninput = (event: any) => {
+    let degree = event.target.value.toString();
+    if (Number(event.target.value) < 10) {
+      degree = '&nbsp;&nbsp;' + degree
+    } else if (Number(event.target.value) < 100) {
+      degree = '&nbsp;' + degree
+    } else {
+      // pass
+    }
+    sowDegreeIndicator.innerHTML = `파종 각도: ${degree}`;
+  }
+
+  if (info.sow) {
+    sowMethodOptions.forEach((option: HTMLOptionElement) => {
+      if (option.value === info.sow.method) {
+        option.setAttribute("selected", "true");
+      } else {
+        option.removeAttribute("selected");
+      }
+    })
+    sowGapOptions.forEach((option: HTMLOptionElement) => {
+      if (option.value === info.sow.gap) {
+        option.setAttribute("selected", "true");
+      } else {
+        option.removeAttribute("selected");
+      }
+    })
+    sowDegreeConfig.value = info.sow.degree.toString();
+    sowDegreeIndicator.innerText = `파종 각도: ${info.sow.degree}`;
+  }
 };
+
+/**==============> Irrigation ================>*/
 
 const registerSelectIrrigationUI = (info: LandTileInfo) => {}
 
+/**<============== End Irrigation <================*/
+
+/**==============> Top Dressing ================>*/
+
 const registerSelectTopDressingUI = (info: LandTileInfo) => {}
+
+/**<============== End Top Dressing <================*/
+
+/**==============> Plowing ================>*/
 
 const registerSelectPlowingButtonUI = (info: LandTileInfo) => {}
 
-registerCropSelectionUI();
+/**<============== End Plowing <================*/
+
+// Main
+
+registerCropSelectionUI({
+  x: [0, 10],
+  y: [0, 10],
+});
 
 const getTimeDom = () => {
   return document.querySelector(".time-text");
